55b069038457203809bfd832bf0c22912227cd3b
Amit77811 <amitdeshmane7781@gmail.com>
2025-12-09 14:06:26 +0530

    Added new Js project

---
+++ movie-search-api/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form');
+const container = document.querySelector('.image-container');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let query = form.querySelector('input').value;
+    console.log(query);
+
+    if(query === ""){
+        alert("Empty input field");
+        return;
+    }
+
+    tvMazeApi(query);
+
+})
+
+async function tvMazeApi(query){
+    const req = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+    const movies = await req.json();
+    makeImages(movies);
+}
+
+function makeImages(movies){
+    container.innerHTML = ""; 
+
+    for(let movie of movies ){
+        let src = movie.show.image.medium;
+
+        const img = document.createElement('img');
+        img.src = src;
+
+        container.appendChild(img);
+    }
+}
